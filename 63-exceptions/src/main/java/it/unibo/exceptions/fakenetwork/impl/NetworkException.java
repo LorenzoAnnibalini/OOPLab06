22ef8ec703c08d010a65883349d84de89eba58b3
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-04 11:34:19 +0100

    Completata classe NetworkException

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/NetworkException.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/NetworkException.java
@@ -3,5 +3,12 @@ package it.unibo.exceptions.fakenetwork.impl;
 import java.io.IOException;
 
 public class NetworkException extends IOException {
-    
+   
+    public NetworkException() throws Exception {
+        throw new Exception("Network error: no response");
+    }
+
+    public NetworkException( final String message) throws Exception {
+        throw new Exception("Network error while sending message: " + message);
+    }
 }
