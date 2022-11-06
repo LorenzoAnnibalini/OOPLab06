8db4926fd8a2640c8441d5ed1aa22e68126ae1f3
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-06 18:22:18 +0100

    Creata classe ImplGraph che estende Graph

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/ImplGraph.java
@@ -0,0 +1,40 @@
+package it.unibo.generics.graph.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+public class ImplGraph<N> implements Graph<N> {
+
+    @Override
+    public void addNode(Object node) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addEdge(Object source, Object target) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set nodeSet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set linkedNodes(Object node) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List getPath(Object source, Object target) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
