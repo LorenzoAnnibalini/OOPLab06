82ac8b0d81791037af04f6f0717fec024955e68c
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-06 20:34:01 +0100

    Cominciato a scrivere i metodi di  ImplGraph

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/ImplGraph.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/ImplGraph.java
@@ -1,39 +1,57 @@
 package it.unibo.generics.graph.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
 
 public class ImplGraph<N> implements Graph<N> {
-
-    @Override
-    public void addNode(Object node) {
-        // TODO Auto-generated method stub
-        
+   
+    private final Map<N, Set<N>> graph;
+    
+    public ImplGraph() {
+        this.graph = new HashMap<N, Set<N>>();
     }
 
     @Override
-    public void addEdge(Object source, Object target) {
-        // TODO Auto-generated method stub
-        
+    public void addNode(final N node) {
+        if(node != null && graph.containsKey(node)) {
+            this.graph.putIfAbsent(node, new HashSet<N>());
+        }
     }
 
     @Override
-    public Set nodeSet() {
-        // TODO Auto-generated method stub
+    public void addEdge(final N source, final N target) {
+        if(source != null && target != null) {
+            this.graph.get(source).add(target);
+        }
+    }
+
+    @Override
+    public Set<N> nodeSet() {
+        return new HashSet<>(this.graph.keySet());
+    }
+
+    @Override
+    public Set<N> linkedNodes(final N node) {
+        if(this.graph.equals(node)) {
+            return new HashSet<>(this.graph.get(node));
+        }
         return null;
     }
 
     @Override
-    public Set linkedNodes(Object node) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public List getPath(Object source, Object target) {
-        // TODO Auto-generated method stub
+    public List<N> getPath(final N source, final N target) {
+     // TODO !!!!!!
+        /*   ArrayList<N> lista;
+        if(source != null && target != null) {
+            
+        }
+    */
         return null;
     }
     
