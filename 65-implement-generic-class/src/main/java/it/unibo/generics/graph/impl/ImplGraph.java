392a9a7fcaba42c38123d19c9efcfe526c23f5c5
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-09 11:13:07 +0100

    Cominciato metodo getPath ImplGraph.java

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/ImplGraph.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/ImplGraph.java
@@ -3,11 +3,14 @@ package it.unibo.generics.graph.impl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
+
+enum Colore {BIANCO, GRIGIO, NERO};
 
 public class ImplGraph<N> implements Graph<N> {
    
@@ -46,12 +49,26 @@ public class ImplGraph<N> implements Graph<N> {
 
     @Override
     public List<N> getPath(final N source, final N target) {
-     // TODO !!!!!!
-        /*   ArrayList<N> lista;
-        if(source != null && target != null) {
-            
+    
+        Map <N,List<N>> mappaBFS = new HashMap<>();
+        Map <N,Colore> mappaColori = new HashMap<>();
+        List<N> queue = new ArrayList<>();
+        LinkedList<N> tmp;
+        N nodoTMP;
+        
+        //preparo la mappaBFS e la mappaColori per ogni nodo
+        for (N nodo : this.nodeSet()){
+            mappaBFS.put(nodo,new LinkedList<>());
+            mappaColori.put(nodo, Colore.BIANCO);
         }
-    */
+
+        //aggiungo source
+        queue.add(source);
+        mappaColori.put(source, Colore.GRIGIO);
+        mappaBFS.put(source, new LinkedList<>());
+
+        //TODO !!!!!!!!!!
+
         return null;
     }
     
